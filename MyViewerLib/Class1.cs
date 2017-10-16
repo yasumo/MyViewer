dcfc220757b30d45213b493ea2bba89f95448e66
yasumo <ndve0027@gmail.com>
2017-10-16 19:35:28 +0900

    sqlite接続まで

--- MyViewerLib/Class1.cs
+++ MyViewerLib/Class1.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
 
 namespace MyViewerLib
 {
-    public class Class1
+
+
+    namespace LinqTest
     {
+        [Table(Name = "TestTable")]
+        public class TTestData
+        {
+            //ID
+            [Column(Name = "Id", DbType = "INT", CanBeNull = true, IsPrimaryKey = false)]
+            public Int32? Id { get; set; }
+
+            // Title
+            [Column(Name = "Title", DbType = "NVARCHAR", CanBeNull = true)]
+            public String Title { get; set; }
+
+            // Detail
+            [Column(Name = "Detail", DbType = "NVARCHAR", CanBeNull = true)]
+            public String Detail { get; set; }
+        }
     }
 }
---
+++ MyViewerStub/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.SQLite;
+using MyViewerLib.LinqTest;
+using System.Data.Linq.Mapping;
+
+namespace MyViewerStub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteConnectionStringBuilder aConnectionString = new SQLiteConnectionStringBuilder
+            {
+                DataSource = @"E:\WindowsWorkFiles\Documents\visual studio 2017\Projects\MyViewer\res\LinqTest.sqlite" // データベースの保存先
+            };
+            using (SQLiteConnection aConnection = new SQLiteConnection(aConnectionString.ToString()))
+            {
+                // データベースを開く
+                aConnection.Open();
+
+                // ここにデータベース処理コードを書く
+                using (DataContext aContext = new DataContext(aConnection))
+                {
+                    Table<TTestData> aTableTest = aContext.GetTable<TTestData>();
+                    IQueryable<TTestData> aQueryResult =
+                        from x in aTableTest
+                        select x;
+
+                    Console.WriteLine("=== test ===");
+                    foreach (TTestData aData in aQueryResult)
+                    {
+                        Console.WriteLine("=== {0} ===", aData.Id);
+                        Console.WriteLine(aData.Title);
+                        Console.WriteLine(aData.Detail);
+                    }
+
+                    Console.ReadLine(); // Enterキー押下でコマンドプロンプトが閉じる
+                }
+                // データベースを閉じる
+                aConnection.Close();
+            }
+        }
+    }
+}
