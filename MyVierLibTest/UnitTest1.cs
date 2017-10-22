a483b62aa007b58b08c487bd3f139f212694194b
yasumo <ndve0027@gmail.com>
2017-10-22 12:42:19 +0900

    テストプロジェクト作成、ディレクトリとiniファイル読み込みのテストを作成

---
+++ MyVierLibTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyViewerLib;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MyVierLibTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string iniPath = @".\test\settings\settings.ini";
+
+        private string getPicDir()
+        {
+            Kernel32 kernel = new Kernel32();
+            var targetDir = kernel.GetIniValue(iniPath, "settings", "PictureDirectory");
+            return targetDir;
+        }
+        [TestMethod]
+        public void IniFileReadTest()
+        {
+            var picDir = getPicDir();
+            Assert.AreEqual(@"E:\WindowsWorkFiles\Documents\visual studio 2017\Projects\MyViewer\MyVierLibTest\test", picDir);
+        }
+
+        [TestMethod]
+        public void FolderReadTest()
+        {
+
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(getPicDir());
+            IEnumerable<System.IO.DirectoryInfo> subFolders =
+                di.EnumerateDirectories("*", System.IO.SearchOption.AllDirectories);
+
+            var count = 0;
+            //サブフォルダを列挙する
+            foreach (System.IO.DirectoryInfo subFolder in subFolders)
+            {
+                count++;
+            }
+
+            Assert.AreEqual(7, count);
+        }
+
+
+    }
+}
--- MyViewerStub/Program.cs
+++ MyViewerStub/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Linq;
 using System.Data.SQLite;
 using MyViewerLib.LinqTest;
-using MyViewerLib;
-using System.Data.Linq.Mapping;
 
 namespace MyViewerStub
 {
@@ -17,29 +13,10 @@ namespace MyViewerStub
         {
             sqltest();
             //foldertest();
-            initest();
             Console.ReadLine(); // Enterキー押下でコマンドプロンプトが閉じる
 
         }
 
-        static void initest()
-        {
-            Kernel32 hoge = new Kernel32();
-            Console.WriteLine(hoge.GetIniValue(@".\settings\settings.ini", "settings", "PictureDirectory"));
-        }
-
-        static void foldertest()
-        {
-            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"E:\data\dropbox\Dropbox\pic");
-            IEnumerable<System.IO.DirectoryInfo> subFolders =
-                di.EnumerateDirectories("*", System.IO.SearchOption.AllDirectories);
-
-            //サブフォルダを列挙する
-            foreach (System.IO.DirectoryInfo subFolder in subFolders)
-            {
-                Console.WriteLine(subFolder.FullName);
-            }
-        }
 
         static void sqltest()
         {
