cbe9f4c55c5042c569fa42d8719ec7e2a10effd3
yasumo <ndve0027@gmail.com>
2018-01-03 11:44:47 +0900

    画面作成

---
+++ MyViewer/Helpers/Observable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+// https://github.com/Microsoft/WindowsTemplateStudio/blob/master/templates/_composition/MVVMBasic/Project/Helpers/Observable.cs から引用
+// 名前空間を修正
+namespace MyViewer.Helpers
+{
+    public class Observable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ MyViewer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyViewer.ViewModels
+{
+    public class MainPageViewModel : Helpers.Observable
+    {
+        public Views.MainPage View { get; private set; } = null;
+
+        public void Initialize(Views.MainPage mainPage)
+        {
+            View = mainPage;
+        }
+    }
+}
